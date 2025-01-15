5d57abaf43359965057ef1de775b04aedf2e3408
DanielLS12 <109062103+DanielLS12@users.noreply.github.com>
2025-01-15 01:22:15 -0500

    feat: Habilitar uso de Bearer Token en Swagger para autenticación

--- Configuration/SwaggerConfiguration.cs
+++ Configuration/SwaggerConfiguration.cs
@@ -11,12 +11,46 @@ namespace ViteMontevideo_API.Configuration
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Title = "ViteMontevideo",
+                    Title = "Estacionamiento Montevideo",
                     Version = "v1",
-                    Description = "API para ViteMontevideo (.NET MAUI)"
+                    Description = @"API para la gestión de un estacionamiento, 
+                    diseñada para facilitar el control de vehículos, espacios disponibles y tarifas. 
+                    Permite registrar ingresos y salidas de vehículos, calcular el costo por tiempo de permanencia,
+                    y generar reportes de ocupación e ingresos.
+
+                    Incluye autenticación por roles para garantizar un acceso seguro y controlado,
+                    y permite la integración con sistemas externos para la sincronización de datos."
                 });
 
                 c.SchemaFilter<EnumSchemaFilter>();
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Autorización",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Por favor, ingresar un token válido con el siguiente formato: {tu token aquí} no agregar la palabra 'Bearer' antes."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
     }
